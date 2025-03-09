ffd7c037bbe4ffc2a14b610cd03cd9309772fd27
Kelvin Young <kelvinloyyipyoung@gmail.com>
2025-03-09 17:41:33 +1100

    add dylan project

--- static/content/projects.py
+++ static/content/projects.py
@@ -6,5 +6,12 @@ PROJECTS = [
         'github_link': 'https://github.com/kelvinloyyipyoung/journey',
         'live_link': 'https://journey-kelvinyoung.up.railway.app',
     },
+    {
+        'title': 'dylan',
+        'description': 'A tool for finding factory codes from product names, used for my part time job.',
+        'technologies': ['Python', 'Flask', 'HTML', 'CSS', 'Gemini API'],
+        'github_link': 'https://github.com/kelvinloyyipyoung/dylan',
+        'live_link': 'https://dylan-production.up.railway.app',
+    },
     # Add more projects here
 ]
